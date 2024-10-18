53483e0a374ebe22fdc8600e0339edece9e6d52c
dcardon <dcardon@DESKTOP-8G8NQVR>
2024-10-18 16:01:24 -0300

    Seteo de enviroment y ramas de nuevo repo para actualizar con el resto de cambios.

--- Dashboard.Designer.cs
+++ Dashboard.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace SistemaGestionLab
 {
-    partial class Form1
+    partial class Dashboard
     {
         /// <summary>
         /// Variable del diseñador necesaria.
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Dashboard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Name = "Dashboard";
+            this.Text = "Dashboard";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace SistemaGestionLab
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Dashboard());
         }
     }
 }
