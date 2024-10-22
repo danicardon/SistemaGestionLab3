ae12d82fab3172884bbbe4ba1f5a0e112c656a9d
Alumno <Alumno@PC214>
2024-10-22 15:52:48 -0300

    agrego formularios 1

---
+++ FrmCompras.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace SistemaGestionLab
+{
+    partial class FrmCompras
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menuStrip2 = new System.Windows.Forms.MenuStrip();
+            this.MenuRegistro = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuInventario = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuHistorial = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuPagos = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.menuStrip1.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(18, 18);
+            this.menuStrip1.Location = new System.Drawing.Point(0, 38);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 6;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // menuStrip2
+            // 
+            this.menuStrip2.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.menuStrip2.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.menuStrip2.ImageScalingSize = new System.Drawing.Size(18, 18);
+            this.menuStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.MenuRegistro,
+            this.menuInventario,
+            this.menuHistorial,
+            this.MenuPagos});
+            this.menuStrip2.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip2.Name = "menuStrip2";
+            this.menuStrip2.Size = new System.Drawing.Size(800, 38);
+            this.menuStrip2.TabIndex = 7;
+            this.menuStrip2.Text = "menuStrip2";
+            // 
+            // MenuRegistro
+            // 
+            this.MenuRegistro.Name = "MenuRegistro";
+            this.MenuRegistro.Size = new System.Drawing.Size(132, 34);
+            this.MenuRegistro.Text = "REGISTRO ";
+            // 
+            // menuInventario
+            // 
+            this.menuInventario.Name = "menuInventario";
+            this.menuInventario.Size = new System.Drawing.Size(250, 34);
+            this.menuInventario.Text = "GESTION INVENTARIO";
+            // 
+            // menuHistorial
+            // 
+            this.menuHistorial.Name = "menuHistorial";
+            this.menuHistorial.Size = new System.Drawing.Size(134, 34);
+            this.menuHistorial.Text = "HISTORIAL";
+            // 
+            // MenuPagos
+            // 
+            this.MenuPagos.Name = "MenuPagos";
+            this.MenuPagos.Size = new System.Drawing.Size(95, 34);
+            this.MenuPagos.Text = "PAGOS";
+            // 
+            // FrmCompras
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.menuStrip2);
+            this.Name = "FrmCompras";
+            this.Text = "FrmCompras";
+            this.menuStrip2.ResumeLayout(false);
+            this.menuStrip2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.MenuStrip menuStrip2;
+        private System.Windows.Forms.ToolStripMenuItem MenuRegistro;
+        private System.Windows.Forms.ToolStripMenuItem menuInventario;
+        private System.Windows.Forms.ToolStripMenuItem menuHistorial;
+        private System.Windows.Forms.ToolStripMenuItem MenuPagos;
+    }
+}
---
+++ frmInicio.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace SistemaGestionLab
+{
+    partial class frmInicio
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuiNICIO = new System.Windows.Forms.MenuStrip();
+            this.crearCuentaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ventasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.registroDeVentaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.historialDeVentasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.estadísticasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.comprasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.proveedoresToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.agendaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuiNICIO.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuiNICIO
+            // 
+            this.menuiNICIO.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.menuiNICIO.Font = new System.Drawing.Font("Arial Black", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.menuiNICIO.ImageScalingSize = new System.Drawing.Size(18, 18);
+            this.menuiNICIO.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.crearCuentaToolStripMenuItem,
+            this.ventasToolStripMenuItem,
+            this.comprasToolStripMenuItem,
+            this.proveedoresToolStripMenuItem,
+            this.agendaToolStripMenuItem});
+            this.menuiNICIO.Location = new System.Drawing.Point(0, 0);
+            this.menuiNICIO.Name = "menuiNICIO";
+            this.menuiNICIO.Size = new System.Drawing.Size(800, 38);
+            this.menuiNICIO.TabIndex = 1;
+            this.menuiNICIO.Text = "menuStrip1";
+            // 
+            // crearCuentaToolStripMenuItem
+            // 
+            this.crearCuentaToolStripMenuItem.Name = "crearCuentaToolStripMenuItem";
+            this.crearCuentaToolStripMenuItem.Size = new System.Drawing.Size(173, 34);
+            this.crearCuentaToolStripMenuItem.Text = "Crear cuenta";
+            // 
+            // ventasToolStripMenuItem
+            // 
+            this.ventasToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.registroDeVentaToolStripMenuItem,
+            this.historialDeVentasToolStripMenuItem,
+            this.estadísticasToolStripMenuItem});
+            this.ventasToolStripMenuItem.Name = "ventasToolStripMenuItem";
+            this.ventasToolStripMenuItem.Size = new System.Drawing.Size(104, 34);
+            this.ventasToolStripMenuItem.Text = "Ventas";
+            // 
+            // registroDeVentaToolStripMenuItem
+            // 
+            this.registroDeVentaToolStripMenuItem.Name = "registroDeVentaToolStripMenuItem";
+            this.registroDeVentaToolStripMenuItem.Size = new System.Drawing.Size(305, 34);
+            this.registroDeVentaToolStripMenuItem.Text = "Registro de Venta";
+            // 
+            // historialDeVentasToolStripMenuItem
+            // 
+            this.historialDeVentasToolStripMenuItem.Name = "historialDeVentasToolStripMenuItem";
+            this.historialDeVentasToolStripMenuItem.Size = new System.Drawing.Size(305, 34);
+            this.historialDeVentasToolStripMenuItem.Text = "Historial de Ventas";
+            // 
+            // estadísticasToolStripMenuItem
+            // 
+            this.estadísticasToolStripMenuItem.Name = "estadísticasToolStripMenuItem";
+            this.estadísticasToolStripMenuItem.Size = new System.Drawing.Size(305, 34);
+            this.estadísticasToolStripMenuItem.Text = "Estadísticas";
+            // 
+            // comprasToolStripMenuItem
+            // 
+            this.comprasToolStripMenuItem.Name = "comprasToolStripMenuItem";
+            this.comprasToolStripMenuItem.Size = new System.Drawing.Size(126, 34);
+            this.comprasToolStripMenuItem.Text = "Compras";
+            // 
+            // proveedoresToolStripMenuItem
+            // 
+            this.proveedoresToolStripMenuItem.Name = "proveedoresToolStripMenuItem";
+            this.proveedoresToolStripMenuItem.Size = new System.Drawing.Size(166, 34);
+            this.proveedoresToolStripMenuItem.Text = "Proveedores";
+            // 
+            // agendaToolStripMenuItem
+            // 
+            this.agendaToolStripMenuItem.Name = "agendaToolStripMenuItem";
+            this.agendaToolStripMenuItem.Size = new System.Drawing.Size(111, 34);
+            this.agendaToolStripMenuItem.Text = "Agenda";
+            // 
+            // frmInicio
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuiNICIO);
+            this.Name = "frmInicio";
+            this.Text = "frmInicio";
+            this.menuiNICIO.ResumeLayout(false);
+            this.menuiNICIO.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuiNICIO;
+        private System.Windows.Forms.ToolStripMenuItem crearCuentaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ventasToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem registroDeVentaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem historialDeVentasToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem estadísticasToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem comprasToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem proveedoresToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem agendaToolStripMenuItem;
+    }
+}
